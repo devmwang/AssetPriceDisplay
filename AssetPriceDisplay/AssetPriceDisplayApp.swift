dfdc64719fabae935f790cca68693b62f183840b
Michael Wang <41971562+devmwang@users.noreply.github.com>
2022-06-07 12:25:15 -0700

    Initial Commit

---
+++ AssetPriceDisplay/AssetPriceDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetPriceDisplayApp.swift
+//  AssetPriceDisplay
+//
+//  Created by Michael Wang on 2022-06-07.
+//
+
+import SwiftUI
+
+@main
+struct AssetPriceDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
